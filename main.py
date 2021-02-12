7e00f527b6ec52cad0e4a56db784cb6ca18dc76c
Alexander Zakharchuk <mystang.pro@gmail.com>
2021-02-12 22:13:24 +0300

    Circles are now drawn by one

--- main.py
+++ main.py
@@ -1,32 +1,31 @@
-from PyQt5 import QtWidgets, uic, QtGui
-from PyQt5.QtCore import Qt
 import random
 import sys
+from PyQt5 import QtWidgets, uic, QtGui
+from PyQt5.QtCore import Qt
 
 
 class MainWindow(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        self.allow_paint = False
+        self.circles = []
         self.setup_ui()
 
     def setup_ui(self):
         uic.loadUi('Ui.ui', self)
         self.setWindowTitle("Git и желтые окружности")
-        self.button.clicked.connect(self.set_paint_active)
+        self.button.clicked.connect(self.redraw_circles)
 
     def paintEvent(self, event):
         painter = QtGui.QPainter(self)
         color = QtGui.QColor(255, 255, 0)
-        size = random.randint(50, 200)
         painter.setBrush(QtGui.QBrush(color, Qt.SolidPattern))
-        for _ in range(random.randint(1, 10)):
-            painter.drawEllipse(random.randint(10, 300),
-                                random.randint(10, 300), size, size)
-        self.allow_paint = False
+        for circle in self.circles:
+            painter.drawEllipse(*circle)
 
-    def set_paint_active(self):
-        self.allow_paint = True
+    def redraw_circles(self):
+        size = random.randint(50, 200)
+        self.circles.append((random.randint(10, 300),
+                             random.randint(10, 300), size, size))
         self.update()
 
 
